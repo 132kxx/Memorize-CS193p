83f229fe2ba55a3bca0984272d4814676e8efee5
kxx <132kxx@gmail.com>
2022-10-04 12:08:02 +0900

    Initial Commit

---
+++ Memorize-CS193p/Memorize_CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_CS193pApp.swift
+//  Memorize-CS193p
+//
+//  Created by kxx: on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
